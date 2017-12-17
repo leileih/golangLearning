3ef791db479ed117dcb56e8dd88d5d062ee84fbf
HuLeilei <huleilei@HuLeileis-MacBook-Pro.local>
2017-12-18 00:04:40 +0800

    接收 sigterm 信号

--- crawler/1crawlerQQ.go
+++ crawler/1crawlerQQ.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"practice/golangLearning/links"
 	"sync"
 )
@@ -41,13 +42,42 @@ func crawl(url string, depth int, wg *sync.WaitGroup) {
 }
 
 func main() {
+	// c := make(chan os.Signal, 1)
+	// signal.Notify(c, syscall.SIGTERM, syscall.SIGTRAP)
+	// Block until a signal is received.
+	// fmt.Println("启动")
+	// s := <-c
+	// fmt.Println("Got signal: ", s)
+
+	// flag 包实现了命令行参数的解析
 	flag.IntVar(&maxDepth, "d", 2, "max crawl depth")
 	flag.Parse()
 	wg := &sync.WaitGroup{}
 	for _, link := range flag.Args() {
-		fmt.Println("used for breakpoint")
+		// tlink := link
+		//fmt.Println("used for breakpoint")
 		wg.Add(1)
+		// go func() {
+		// 	fmt.Println("start go func")
+		// 	for s := range c {
+		// 		fmt.Println("start range chan")
+		// 		switch s {
+		// 		case syscall.SIGTERM, syscall.SIGTRAP:
+		// 			fmt.Println("退出", s)
+		// 			exitFunc()
+		// 		}
+		// 	}
+		// 	crawl(tlink, 0, wg)
+		// }()
 		go crawl(link, 0, wg)
 	}
 	wg.Wait()
+}
+
+// method after received SIGTERM
+func exitFunc() {
+	fmt.Println("开始退出...")
+	fmt.Println("执行清理...")
+	fmt.Println("结束退出...")
+	os.Exit(0)
 }
---
+++ test/test.go
@@ -0,0 +1,95 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"os"
+// 	"os/signal"
+// 	"syscall"
+// )
+
+// // 监听指定信号
+// func main() {
+// 	//合建chan
+// 	c := make(chan os.Signal)
+// 	//监听指定信号 ctrl+c kill
+// 	signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGTERM, syscall.SIGHUP)
+// 	//阻塞直到有信号传入
+// 	fmt.Println("启动")
+
+// 	//阻塞直至有信号传入
+// 	s := <-c
+// 	fmt.Println("退出信号", s)
+// }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"os"
+// 	"os/signal"
+// 	"syscall"
+// )
+
+// func main() {
+// 	signalChan := make(chan os.Signal, 1)
+// 	signal.Notify(signalChan,
+// 		syscall.SIGHUP,
+// 		syscall.SIGINT,
+// 		syscall.SIGTERM,
+// 		syscall.SIGQUIT)
+
+// 	exitChan := make(chan int)
+// 	go func() {
+// 		for {
+// 			s := <-signalChan
+// 			switch s {
+// 			// kill -SIGHUP XXXX
+// 			case syscall.SIGHUP:
+// 				fmt.Println("Got signal: hungup")
+
+// 			// kill -SIGINT XXXX or Ctrl+c
+// 			case syscall.SIGINT:
+// 				fmt.Println("Warikomi")
+
+// 			// kill -SIGTERM XXXX
+// 			case syscall.SIGTERM:
+// 				fmt.Println("force stop")
+// 				exitChan <- 0
+
+// 			// kill -SIGQUIT XXXX
+// 			case syscall.SIGQUIT:
+// 				fmt.Println("stop and core dump")
+// 				exitChan <- 0
+
+// 			default:
+// 				fmt.Println("Unknown signal.")
+// 				exitChan <- 1
+// 			}
+// 		}
+// 	}()
+
+// 	code := <-exitChan
+// 	os.Exit(code)
+// }
+
+// example from golang.org
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	// Set up channel on which to send signal notifications.
+	// We must use a buffered channel or risk missing the signal
+	// if we're not ready to receive when the signal is sent.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	// Block until a signal is received.
+	s := <-c
+	fmt.Println("Got signal:", s)
+}
